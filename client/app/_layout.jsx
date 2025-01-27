862a27cddbc3d042cbeedcd510b2758b5c3654c3
AdwaithAnanfSr <adwaithanand1818@gmail.com>
2025-01-27 15:09:02 +0530

    added home directory

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { View } from "react-native";
 
 import TrackController from "../components/TrackController.jsx";
-
+import Navbar from "../components/Navbar.jsx";
 import { ThemeProvider } from "../context/theme.context.js";
 import { ListProvider } from "../context/list.context.js";
 import { TrackProvider } from "../context/track.context.js";
@@ -15,6 +15,7 @@ const _layout = () => {
           <View
             style={{ width: "100%", height: "100%", backgroundColor: "black" }}
           >
+            <Navbar />
             <Stack screenOptions={{ headerShown: false }}>
               <Stack.Screen name="index" />
               <Stack.Screen name="home" />
