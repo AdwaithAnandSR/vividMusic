3780e7537e2c7823a43310090079aefc2bfbe38c
AdwaithAnandSR <adwaithanandsr@users.noreply.github.com>
2025-02-28 09:36:35 +0530

    added search query

--- server/handlers/search.handler.js
+++ server/handlers/search.handler.js
@@ -1,12 +1,44 @@
-
 const { getIo } = require("../config/socket.config.js");
 const musicModel = require("../models/musics.js");
 const io = getIo();
 
+const searchSongs = async query => {
+   try {
+      const words = query.toLowerCase().split(" ");
 
-io.on("connection", (socket) => {
-  socket.on("searchSongs", async (text) => {
-    const songs = await musicModel.find({});
-    console.log(songs);
-  });
+      // Create search conditions with both singular and plural forms
+      const mustConditions = words.flatMap(word => [
+         { text: { query: word, path: "title", fuzzy: { maxEdits: 1 } } },
+         {
+            text: {
+               query: word.replace(/s$/, ""),
+               path: "title",
+               fuzzy: { maxEdits: 1 }
+            }
+         } // Remove trailing 's'
+      ]);
+
+      const result = await musicModel.aggregate([
+         {
+            $search: {
+               index: "title",
+               compound: {
+                  must: mustConditions
+               }
+            }
+         }
+      ]);
+      return result;
+   } catch (error) {
+      console.error("Search error:", error);
+      return [];
+   }
+};
+
+io.on("connection", socket => {
+   socket.on("searchSongs", async text => {
+      const songs = searchSongs(text);
+      console.log(songs);
+      socket.emit('searchSongsRes', songs)
+   });
 });
--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -4,27 +4,8 @@ const router = express.Router();
 const musicModel = require("../models/musics.js");
 const { signin, signup } = require("../handlers/auth.handler.js");
 
-//global routes
-router.post("/getGlobalSongs", async (req, res) => {
-   try {
-      const { limit, page } = req.body;
-
-      const musics = await musicModel
-         .find({})
-         .sort({ createdAt: -1 })
-         .skip((page - 1) * limit)
-         .limit(limit);
-
-      if (musics) return res.status(200).json({ musics });
-   } catch (e) {
-      console.error(e);
-   }
-});
-
 router.get("/", async (req, res) => {
-   const musics = await musicModel.find({});
-   console.log('/ route');
-   res.send(musics);
+   res.send('heloo');
 });
 
 // authentication routes
