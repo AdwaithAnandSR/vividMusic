1411273d00f88896d9c016ae7030dd47f8f3c6c4
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-03-10 18:50:26 +0530

    ..

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -5,30 +5,35 @@ const musicModel = require("../models/musics.js");
 const { signin, signup } = require("../handlers/auth.handler.js");
 
 router.get("/", async (req, res) => {
-   res.send('heloo');
+   res.send("heloo");
 });
 
-router.post("/getGlobalSongs", async (req, res)=>{
-   try{
-      const {limit, page} = req.body;
-      const musics = await musicModel.find({}).sort({createdAt: -1}).skip((page-1)*limit).limit(limit);
+router.post("/getGlobalSongs", async (req, res) => {
+   try {
+      const { limit, page } = req.body;
+      const musics = await musicModel
+         .find({})
+         .sort({ createdAt: -1 })
+         .skip((page - 1) * limit)
+         .limit(limit);
 
-      if(musics) return res.status(200).json({ musics });
-   }catch(err){
+      if (musics) return res.status(200).json({ musics });
+   } catch (err) {
       console.error("error while fetching songs: ", error);
    }
 });
 
 router.post("/searchSong", async (req, res) => {
-  try {
-    const songs = await musicModel.find({
-      title: { $regex: query, $options: "i" }, // i : case-insensitive
-    });
+   const { text } = req.body;
+   try {
+      const songs = await musicModel.find({
+         title: { $regex: text, $options: "i" } // i : case-insensitive
+      });
 
-    res.json({songs});
-  } catch (error) {
-    console.error("Search error:", error);
-  }
+      res.json({ songs });
+   } catch (error) {
+      console.error("Search error:", error);
+   }
 });
 
 // authentication routes
