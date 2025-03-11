fb5b5ac6cfc2fb5f81d0062ecc5f1a6028af5b8d
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-03-11 08:32:50 +0530

    ..

--- client/hooks/useSearch.js
+++ client/hooks/useSearch.js
@@ -5,23 +5,26 @@ import axios from "axios";
 const api = Constants.expoConfig.extra.clientApi;
 
 const useSearch = ({ text, setList }) => {
-  const [songs, setSongs] = useState([]);
+   const [songs, setSongs] = useState([]);
 
-  const fetchSongs = async () => {
-    const res = await axios.post(`${api}/searchSong`, text);
-    if (res?.data?.songs?.length > 0) setSongs(res?.data?.songs);
-  };
+   const fetchSongs = async () => {
+      console.log("hu");
+      const res = await axios.post(`${api}/searchSong`, { text });
+      console.log(res.data);
+      if (res?.data?.songs?.length > 0) setSongs(res?.data?.songs);
+   };
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (!text || text.trim() == "") setSongs([]);
-      else fetchSongs();
-    }, 500);
+   useEffect(() => {
+      const timeout = setTimeout(() => {
+         if (!text || text.trim() == "") setSongs([]);
+         else fetchSongs();
+      }, 500);
+      console.log(text);
 
-    return () => clearTimeout(timeout);
-  }, [text]);
+      return () => clearTimeout(timeout);
+   }, [text]);
 
-  return { songs, setSongs };
+   return { songs, setSongs };
 };
 
 export default useSearch;
